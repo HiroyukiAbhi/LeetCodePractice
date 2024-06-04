7aeea08df9b1886e0f53b187abdd5b98f86baf41
HiroyukiAbhi <intelligence2003@gmail.com>
2024-06-03 23:48:43 -0700

    Fixed edge case another edge case with an update on using a single pass -- much smarter

--- TwoSum_1.py
+++ TwoSum_1.py
@@ -2,9 +2,20 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash = {}
         for index,i in enumerate(nums):
+            if i in hash:
+
             hash[i] = index
         for i in hash:
             result = target - i
             if result in hash and hash[result] != hash[i]:
                 return [hash[i], hash[result]]
 
+    def twoSumOnePass(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for index, value in enumerate(nums):
+            result = target - value
+            if result in hash and index != hash[result]:
+                return [index, hash[result]]
+            hash[value] = index
+
+        return []
