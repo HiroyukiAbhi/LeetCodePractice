67866453cbb89a3a8f1a1797986357bb64036e81
HiroyukiAbhi <intelligence2003@gmail.com>
2024-06-06 23:26:41 -0700

    FINISHED PROBLEM GROUP ANAGRAMS

--- anagram_groups_49.py
+++ anagram_groups_49.py
@@ -1,14 +1,10 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash = {}
-        hashedList = []
-        out = []
-        for index, value in enumerate(strs):
-            a = {}
-            for i, v in enumerate(value):
-                if v in a:
-                    a[v] += 1
-                else:
-                    a[v] = 1
-            if a in hashedList:
 
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = defaultdict(list)
+        for stri in strs:
+            chars = [0] * 26
+            for i in stri:
+                chars[ord(i) - ord("a")]
+            hash[tuple(chars)].append(stri)
+        return hash.values
